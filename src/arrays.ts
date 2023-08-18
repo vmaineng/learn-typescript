3a50fbf200236fd93601531059471750f97763d5
Mai Vang <vmaineng@gmail.com>
2023-08-17 17:18:15 -0700

    added in arrays

---
+++ src/arrays.ts
@@ -0,0 +1,12 @@
+// let numbers = [1, 2, 3];
+// let numbers = [1, 2, '3']; //valid in JS
+
+//let numbers:number[] = [1, 2, 3]; //stating number array TS
+
+//let numbers = []; //any array which we should avoid;
+
+let numbers: number[] = []; //have to state number array
+
+//intellisense - can see all the methods listed
+
+numbers.forEach(n => n.)

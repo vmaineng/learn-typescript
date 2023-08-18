df490968656121c5a9560b0b9bd41d5a630dcf62
Mai Vang <vmaineng@gmail.com>
2023-08-17 17:48:59 -0700

    functions added

--- src/functions.ts
+++ src/functions.ts
@@ -1,7 +1,14 @@
-function calculateTax(income: number): number { //stating we expect a number for a return
-    return 0;
+function calculateTax(income: number, taxYear: number): number { //stating we expect a number for a return
+
+    if (taxYear < 2022) 
+    return income * 1.5
+    return income * 1.3;
+   
+   // return 0; //if return "a", it would be a compilation error
 }
 
+calculateTax(10_000, 2022)
 
 //always properly annonate for functions
 //specificy return type
+
